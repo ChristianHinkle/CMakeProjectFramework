abb6b051fc8954124828854c1808be2c70d112e9
Christian Hinkle <christianhinko@gmail.com>
2026-03-27 04:50:08 -0600

    [cmake][code_migration] Bring in static/shared/module/object project code in the form of inline cmake code modules and macros (copied from MeddySDK_Meddyproject's files).

--- CMakeProjectFramework.cmake
+++ CMakeProjectFramework.cmake
@@ -34,3 +34,43 @@ macro(cmakeprojectframework_cmakelists_sourceproject base_project_name_full base
   include(InlineCode/CMakeLists_SourceProject)
 
 endmacro()
+
+macro(cmakeprojectframework_cmakelists_staticproject base_project_name_full base_project_name_namespace base_project_name_leafname)
+
+  set(MY_BASE_PROJECT_NAME_FULL ${base_project_name_full})
+  set(MY_BASE_PROJECT_NAME_NAMESPACE ${base_project_name_namespace})
+  set(MY_BASE_PROJECT_NAME_LEAFNAME ${base_project_name_leafname})
+
+  include(InlineCode/CMakeLists_StaticProject)
+
+endmacro()
+
+macro(cmakeprojectframework_cmakelists_sharedproject base_project_name_full base_project_name_namespace base_project_name_leafname)
+
+  set(MY_BASE_PROJECT_NAME_FULL ${base_project_name_full})
+  set(MY_BASE_PROJECT_NAME_NAMESPACE ${base_project_name_namespace})
+  set(MY_BASE_PROJECT_NAME_LEAFNAME ${base_project_name_leafname})
+
+  include(InlineCode/CMakeLists_SharedProject)
+
+endmacro()
+
+macro(cmakeprojectframework_cmakelists_moduleproject base_project_name_full base_project_name_namespace base_project_name_leafname)
+
+  set(MY_BASE_PROJECT_NAME_FULL ${base_project_name_full})
+  set(MY_BASE_PROJECT_NAME_NAMESPACE ${base_project_name_namespace})
+  set(MY_BASE_PROJECT_NAME_LEAFNAME ${base_project_name_leafname})
+
+  include(InlineCode/CMakeLists_ModuleProject)
+
+endmacro()
+
+macro(cmakeprojectframework_cmakelists_objectproject base_project_name_full base_project_name_namespace base_project_name_leafname)
+
+  set(MY_BASE_PROJECT_NAME_FULL ${base_project_name_full})
+  set(MY_BASE_PROJECT_NAME_NAMESPACE ${base_project_name_namespace})
+  set(MY_BASE_PROJECT_NAME_LEAFNAME ${base_project_name_leafname})
+
+  include(InlineCode/CMakeLists_ObjectProject)
+
+endmacro()
